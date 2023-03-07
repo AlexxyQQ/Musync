d3b246f0e53929832595fb046d1a7b2f62e7a439
AlexxyQQ <aayushpandey616@gmail.com>
2023-03-07 10:30:16 +0545

    userd on qudio query dependency

--- lib/ui/music/pages/music_list_page.dart
+++ lib/ui/music/pages/music_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:musync/ui/music/provider/music_provider.dart';
+import 'package:on_audio_query/on_audio_query.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class MusicListPage extends StatefulWidget {
   final String directory;
@@ -11,6 +13,7 @@ class MusicListPage extends StatefulWidget {
 }
 
 class _MusicListPageState extends State<MusicListPage> {
+  final OnAudioQuery _audioQuery = OnAudioQuery();
   final int _pageSize = 100;
   List<String>? _files;
   int _currentPage = 0;
@@ -21,6 +24,17 @@ class _MusicListPageState extends State<MusicListPage> {
   void initState() {
     super.initState();
     _fetchFiles(_currentPage);
+    requestPermission();
+  }
+
+  void requestPermission() async {
+    if (!kIsWeb) {
+      final bool permissionStatus = await _audioQuery.permissionsStatus();
+      if (!permissionStatus) {
+        await _audioQuery.permissionsRequest();
+      }
+      setState(() {});
+    }
   }
 
   Future<void> _fetchFiles(int page) async {
@@ -28,30 +42,18 @@ class _MusicListPageState extends State<MusicListPage> {
       _isLoading = true;
     });
 
-    List<String> files = [];
-    if (widget.directory.isNotEmpty) {
-      files = await MusicProvider().getFolderMusic(widget.directory);
-    } else {
-      files = await MusicProvider().getAllMusic();
-    }
+    final List<String> files = widget.directory.isNotEmpty
+        ? await MusicProvider().getFolderMusic(widget.directory)
+        : await MusicProvider().getAllMusic();
 
-    int start = page * _pageSize;
+    final int start = page * _pageSize;
     int end = (page + 1) * _pageSize;
-    if (end > files.length) {
-      end = files.length;
-    }
+    end = end > files.length ? files.length : end;
 
     setState(() {
-      if (_files == null) {
-        _files = files.sublist(start, end);
-      } else {
-        _files!.addAll(files.sublist(start, end));
-      }
-
-      if (end == files.length) {
-        _allLoaded = true;
-      }
-
+      _files ??= files.sublist(start, end);
+      _files!.addAll(files.sublist(start, end));
+      _allLoaded = end == files.length;
       _currentPage = page;
       _isLoading = false;
     });
@@ -63,46 +65,65 @@ class _MusicListPageState extends State<MusicListPage> {
       appBar: AppBar(
         title: const Text('MP3 and ACC Files'),
       ),
-      body: _files == null
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : _files!.isEmpty
-              ? const Center(
-                  child: Text('No files found'),
-                )
-              : NotificationListener<ScrollNotification>(
-                  onNotification: (scrollNotification) {
-                    if (!_allLoaded &&
-                        scrollNotification is ScrollEndNotification &&
-                        scrollNotification.metrics.extentAfter == 0) {
-                      _fetchFiles(_currentPage + 1);
-                    }
-                    return true;
-                  },
-                  child: ListView.separated(
-                    itemCount: _files!.length + (_allLoaded ? 0 : 1),
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                    itemBuilder: (BuildContext context, int index) {
-                      if (index == _files!.length) {
-                        if (_isLoading) {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        } else {
-                          return const SizedBox.shrink();
-                        }
-                      } else {
-                        return ListTile(
-                          title: Text(
-                            _files![index],
-                          ),
-                        );
-                      }
-                    },
-                  ),
-                ),
+      body: _buildList(),
     );
+  }
+
+  Widget _buildList() {
+    if (_files == null) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    } else if (_files!.isEmpty) {
+      return const Center(
+        child: Text('No files found'),
+      );
+    } else {
+      return NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          if (!_allLoaded &&
+              scrollNotification is ScrollEndNotification &&
+              scrollNotification.metrics.extentAfter == 0) {
+            _fetchFiles(_currentPage + 1);
+          }
+          return true;
+        },
+        child: FutureBuilder<List<SongModel>>(
+          future: _audioQuery.querySongs(
+            sortType: null,
+            path: widget.directory,
+            orderType: OrderType.ASC_OR_SMALLER,
+            uriType: UriType.EXTERNAL,
+            ignoreCase: true,
+          ),
+          builder: (context, item) {
+            if (item.data == null) {
+              return const CircularProgressIndicator();
+            } else if (item.data!.isEmpty) {
+              return const Text("Nothing found!");
+            } else {
+              return ListView.builder(
+                itemCount: item.data!.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(item.data![index].title),
+                    subtitle: Text(item.data![index].artist ?? "No Artist"),
+                    trailing: const Icon(Icons.arrow_forward_rounded),
+                    leading: QueryArtworkWidget(
+                      id: item.data![index].id,
+                      type: ArtworkType.AUDIO,
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/music_player',
+                          arguments: item.data![index]);
+                    },
+                  );
+                },
+              );
+            }
+          },
+        ),
+      );
+    }
   }
 }
